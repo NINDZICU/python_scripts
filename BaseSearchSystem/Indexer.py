ac3f3dd1e7da94f4f9e136d94262039b9aa68862
admin <admin@admins-MacBook-Pro.local>
2019-02-28 11:22:10 +0300

    Посаны я сделяль

--- BaseSearchSystem/Indexer.py
+++ BaseSearchSystem/Indexer.py
@@ -2,6 +2,8 @@ import os
 
 words = []
 
+lemms = []
+
 def get_all_lines(fileName):
     with open(os.getcwd() + fileName, "r", encoding="utf-8") as file:
         content = file.readlines()
@@ -10,10 +12,28 @@ def get_all_lines(fileName):
 for number in range(1, 100):
     doc = get_all_lines("/BaseSearchSystem/lem_pages/" + str(number) + "_lem.txt")
     words = words + doc
+    lemms.append(doc)
 
 words_set = list(set(words))
 
-print(words_set)
+# print(words_set)
+
+results = []
 
 for lem in words_set:
+    result = ""
+    for i in range(0, 99):
+        if lem not in lemms[i]:
+            result = result + "0"
+            continue
+        
+        result = result + "1"
+
+    results.append(result)
+
+# print(results)
+
+with open(os.getcwd() + "/BaseSearchSystem/index/index.txt", "a", encoding="utf-8") as file:
+    for line in results:
+        file.write(line + "\n")
     
