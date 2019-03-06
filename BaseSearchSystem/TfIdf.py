2aaf07809d239458164dfe3d7ead787af8cdf40c
Искандер Хакимжанов <i.khakimzhanov@bars-open.ru>
2019-03-06 10:14:28 +0300

    Поправочка

--- BaseSearchSystem/TfIdf.py
+++ BaseSearchSystem/TfIdf.py
@@ -1,6 +1,5 @@
 import os
 import math
-from collections import Counter
 
 words = []
 lemms = []
@@ -15,12 +14,21 @@ for number in range(1, 100):
     words = words + doc
     lemms.append(doc)
 
-word_dict = Counter(words) 
+word_set = set(words) 
+word_dict = {}
+
+for word in word_set:
+    doc_counter = 0
+    for doc_words in lemms:
+        if word in doc_words:
+            doc_counter += 1
+        continue
+    word_dict[word] = doc_counter
 
 results = []
 
-for word in word_dict:
-    result = word + " :"
+for word in word_set:
+    result = word + ": "
     for doc_words in lemms:
        word_count = doc_words.count(word)
        tf = word_count / len(doc_words)
