c6dbf9a7e59e3e4068b10fbe4ec993f2e626f83b
Ilyas <IIMullahmetov@gmail.com>
2019-03-21 11:45:44 +0300

    Толя доделай вывод пж

--- BaseSearchSystem/PageRank.py
+++ BaseSearchSystem/PageRank.py
@@ -13,18 +13,28 @@ ctx.verify_mode = ssl.CERT_NONE
 matrix = []
 
 links = Utilits.get_all_lines('\\BaseSearchSystem\\web_pages\\links.txt')
+for i in range(0, len(links)):
+    links[i] = links[i].split()[1]
 
 for link in links:
-    html = urllib.request.urlopen(link.split()[1], context=ctx).read()
+    html = urllib.request.urlopen(link, context=ctx).read()
     soup = BeautifulSoup(html, features="html5lib")
-    try:
-        all_links = [el['href'] for el in soup.findAll('a')]
-    except KeyError:
-        pass
-
-    matrix.append(list(map(lambda link: 1 if all_links.__contains__(link) else 0, links)))
+    
+    links_in_link = soup.findAll('a')
+    all_links = set()
+    for link in links_in_link:
+        try:
+            href = link.attrs['href']
+            if not href.startswith('http'):
+                href = 'https://ria.ru' + href
+            all_links.add(href)
+        except KeyError:
+            continue
+    matrix.append(list(map(lambda l: 1 if all_links.__contains__(l) else 0, links)))
 
 pagerank_file = open('pagerank.txt', "w")
+
+
 
 for row in matrix:
     pagerank_file.write(''.join(map(str, row))+'\n')
